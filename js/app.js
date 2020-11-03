79111072b94e926cf77a7f035c32fc316ebf5c4c
Yoga Pratama <yogaprttama@gmail.com>
2020-11-04 00:48:10 +0700

    Add index.html
    First Commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/*!
+  * Menu toggle
+  * Dibuat dengan 100% jQuery
+  * Updated at 2020-11-04 23:59:00
+  *
+ */
+
+$(document).ready(function(){
+  $(".menu-toggle").click(function(){
+    $(".auto-hide").toggleClass("show");
+  });
+});
